6d439a0f2d357744789ab56e4cb7be460d156abd
Jyothi_20B01A0121 <20b01a0121@svecw.edu.in>
2022-12-30 23:45:22 +0530

    registration added

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-function hello(){
-    console.log("Hello Node.js");
-}
-hello();
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent="";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+    
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
